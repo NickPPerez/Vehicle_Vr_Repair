0940148de9dcfbe2adee8474a438865daca7a126
fvnger <swaggysquirtle@gmail.com>
2023-11-15 11:11:53 -0500

    Fixed menu
    Fixed it for the final time, added lasers to easily use menu. added sockets for rims

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -7,7 +7,7 @@ public class MenuController : MonoBehaviour
      
     public void StartBtn()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("MainScene");
 
 
     }
