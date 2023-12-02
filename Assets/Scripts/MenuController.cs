f6bd795bfe04bc0741225c673569742ae869e5a8
John K. Mejia <johnkendrickmejia03@gmail.com>
2023-12-02 05:43:06 -0500

    Updated main menu, and return to main menu buttons added,.
    - Added 3 new PNG assets for the buttons on the Main Menu
    
    - Updates Main Menu to have different buttons to load up each scene
    
    - Added a back to main menu button on the tire and oil change scenes
    
    - Reintroduced the ray interactor in Tire Change scene so user can interact with menu (change needed later so it doesnt pick up wheels)
    
    - Modyfied the MenuControler script with 2 new methods, one to load into the main menu and another to load into the oil change scene

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
-     
-    public void StartBtn()
+
+    public void MainMenuBtn()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void WheelChangeBtn()
     {
         SceneManager.LoadScene("TireChange");
+    }
 
-
+    public void OilChangeBtn()
+    {
+        SceneManager.LoadScene("OilChange");
     }
 
     public void ExitBtn()
